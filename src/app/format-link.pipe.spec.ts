39a3c02e51e0e57f5c1bf7a16ea59b6154b40dde
Karol Andrea Piñon Campos <73372591+KarolaPic@users.noreply.github.com>
2021-10-15 13:00:53 -0500

    Delete src directory

--- src/app/format-link.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FormatLinkPipe } from './format-link.pipe';
-
-describe('FormatLinkPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FormatLinkPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
